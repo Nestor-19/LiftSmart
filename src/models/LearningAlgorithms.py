8970fce59014ec3a7a0999164e2e1bada9b567f4
Nestor_Damien <75420711+Nestor-19@users.noreply.github.com>
2024-08-24 14:55:07 -0400

    Implemented decision_tree() to train and test a decision tree model

--- src/models/LearningAlgorithms.py
+++ src/models/LearningAlgorithms.py
@@ -65,3 +65,83 @@ class ClassificationAlgorithms:
             ordered_features.append(best_feature)
             ordered_scores.append(best_perf)
         return selected_features, ordered_features, ordered_score
+    
+    # Apply a decision tree approach for classification upon the training data (with the specified value for
+    # the minimum samples in the leaf, and the export path and files if print_model_details=True)
+    # and use the created model to predict the outcome for both the
+    # test and training set. It returns the categorical predictions for the training and test set as well as the
+    # probabilities associated with each class, each class being represented as a column in the data frame.
+    def decision_tree(
+        self,
+        train_X,
+        train_y,
+        test_X,
+        min_samples_leaf=50,
+        criterion="gini",
+        print_model_details=False,
+        export_tree_path="Example_graphs/Chapter7/",
+        export_tree_name="tree.dot",
+        gridsearch=True,
+    ):
+        # Create the model
+        if gridsearch:
+            tuned_parameters = [
+                {
+                    "min_samples_leaf": [2, 10, 50, 100, 200],
+                    "criterion": ["gini", "entropy"],
+                }
+            ]
+            dtree = GridSearchCV(
+                DecisionTreeClassifier(), tuned_parameters, cv=5, scoring="accuracy"
+            )
+        else:
+            dtree = DecisionTreeClassifier(
+                min_samples_leaf=min_samples_leaf, criterion=criterion
+            )
+
+        # Fit the model
+
+        dtree.fit(train_X, train_y.values.ravel())
+
+        if gridsearch and print_model_details:
+            print(dtree.best_params_)
+
+        if gridsearch:
+            dtree = dtree.best_estimator_
+
+        # Apply the model
+        pred_prob_training_y = dtree.predict_proba(train_X)
+        pred_prob_test_y = dtree.predict_proba(test_X)
+        pred_training_y = dtree.predict(train_X)
+        pred_test_y = dtree.predict(test_X)
+        frame_prob_training_y = pd.DataFrame(
+            pred_prob_training_y, columns=dtree.classes_
+        )
+        frame_prob_test_y = pd.DataFrame(pred_prob_test_y, columns=dtree.classes_)
+
+        if print_model_details:
+            ordered_indices = [
+                i[0]
+                for i in sorted(
+                    enumerate(dtree.feature_importances_),
+                    key=lambda x: x[1],
+                    reverse=True,
+                )
+            ]
+            print("Feature importance decision tree:")
+            for i in range(0, len(dtree.feature_importances_)):
+                print(
+                    train_X.columns[ordered_indices[i]],
+                )
+                print(
+                    " & ",
+                )
+                print(dtree.feature_importances_[ordered_indices[i]])
+            tree.export_graphviz(
+                dtree,
+                out_file=export_tree_path + export_tree_name,
+                feature_names=train_X.columns,
+                class_names=dtree.classes_,
+            )
+
+        return pred_training_y, pred_test_y, frame_prob_training_y, frame_prob_test_y
